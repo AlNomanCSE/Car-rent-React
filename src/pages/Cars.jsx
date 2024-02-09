7f13c49297a6dc1bb1fa6048374cbe60d63282f5
Abdullah Al Noman <abdullahalnomancse@gmail.com>
2024-02-09 14:27:07 +0600

    cars added to car page

--- src/pages/Cars.jsx
+++ src/pages/Cars.jsx
@@ -1,7 +1,43 @@
-import React from "react";
-
+import React, { useEffect } from "react";
+import "./Cars.modules.css";
+import cars from "../server";
 const Cars = () => {
-  return <div>Cars</div>;
+  return (
+    <>
+      <div className="carpage">
+        <div className="heading">
+          <h1>
+            <h3>Explore our van options</h3>
+          </h1>
+          <div className="filterDiv">
+            <button>Simple</button>
+            <button>Luxury</button>
+            <button>Rugged</button>
+            <button className="clearbtn">Clear filters</button>
+          </div>
+        </div>
+        <div className="displayCars">
+          {cars.map((car) => (
+            <div className="card">
+              <div
+                className="image"
+                style={{ backgroundImage: `url(${car.imageUrl})` }}
+              ></div>
+              <div className="nameNbill">
+                <div className="name">{car.name}</div>
+                <div className="bill">
+                  ${car.price} <p className="perday">/day</p>
+                </div>
+              </div>
+              <div className="type">
+                <button>{car.type}</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Cars;
